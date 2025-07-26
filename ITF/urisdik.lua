24402095a5eda91d7a51c42ce2a90acda6981ead
SteelBall.Adventure <easyfound@126.com>
2025-07-26 19:19:26 +0800

    吟

--- ITF/urisdik.lua
+++ ITF/urisdik.lua
@@ -1,41 +1,39 @@
-﻿
-Analytical:历史 萨尼克 克莱克赛 Transback Hentrunsik
-DBCY:10-13
+﻿DBCY:10-13历史 萨尼克 克莱克赛
         真挚的爱 友情
         忘年交
-        Lost contact  
+        Lost contact
         托梦
-        从战争.杨修到指挥.曹操 ?= 厚黑 
+        从战争.杨修到指挥.曹操 ?= 厚黑
 TriangleTopUnder：1 ：10-14
         母 找 我 女 氏
         35 东边Serial 2次多
         鼻子Arnold 软骨 脚软骨自然歪
-        不记仇 
+        不记仇
         Time
             伊甸Road求救信号：
             Directive：
             --------------------
-            Cowrapped：复仇 细胞 
+            Cowrapped：复仇 细胞
             --------------------
         Snic
         左脚踝above 永远不分离？：东中西女Nice Jealous Dynamically Rotate； 我
         High heel nice yinyang
 Legend
 Legmend
-Rhea CKronor
+R' CKronor
     Vesaid
         GLL
         阿勒泰 KLH
         Range:
-            东方的声音 
-            中 
+            东方的声音
+            中
             西方的声音
         Transtick Rant 好似把声音圈起来
 　　am， amor， amat=love，表示“爱，情爱” amatory 恋爱的；色情的(amat+ory→爱的 )
         amiable 和蔼的，友善的(ami+able能…的→能〔被〕爱的→亲切的) amicable 友好的，温和的(am+ic+able→能爱〔你〕的→友好的) amity 友好，
         和睦(am+ity状态→爱的状态→和睦) enmity 敌意，仇恨(en进入+am+ity→合成enmity，爱太多的成为恨)
 
-   氏族 头发KansaiF 镶饰 
+   氏族 头发KansaiF 镶饰
         仇 磁欧石 折裂斯
         补天
         造人
@@ -43,7 +41,7 @@ Rhea CKronor
     hand palm three texture
 10-20:8 FishHit?.IsFrameStep2:Stil Nodoff
 
-    刑 疼 虫子 gaphole 
+    刑 疼 虫子 gaphole
     轩辕 蚩尤 东夷 秦始皇
     3的问题
     Absolute Undigable
@@ -51,32 +49,32 @@ Rhea CKronor
     起因：声明体.野蛮.文明
         野蛮：蒙昧、陋习、粗野
         维今.模板
-            老掌沟 回龙 启明 公司        
+            老掌沟 回龙 启明 公司
         声明体模板
             相互经验
-            
+
       生命体文明历史最早演化  恐龙骨架 // Dino
-        群组：群 族 氏.世袭 部落 酋 
-          奴 主 隶 封建  //Group        
+        群组：群 族 氏.世袭 部落 酋
+          奴 主 隶 封建  //Group
         克鲁兹：人因水 钨银水 //term.html kause.html
           人类：女 男  义~ 同性~单性~双性
           动物：公 母
-          未知形态： 人能看见 不能看见 生命形态         
-            标量 标杆 
-            靓美好 //waterly  remarkable  beauty                   
+          未知形态： 人能看见 不能看见 生命形态
+            标量 标杆
+            靓美好 //waterly  remarkable  beauty
            起源保护 钱.物 权.名声 地盘 \\宗教影响干预
         源起.从远古是否概括
- 
+
       //Horde.Actioned
         //Airfled.Coregeted
         //Renliy.Killed.Animals.AirLead
         组合//Renliy
           群族：交易：口角
-          奴隶：人肉      
+          奴隶：人肉
           封建：
-        
+
         正面 中和  负面.仇恨源起
-         
+
     总结.补充
 
     涵数.疼：启示录
@@ -85,18 +83,18 @@ Rhea CKronor
         历史：
             问题
             转制
-       Absolute.Renly.Rate.Range.Undigable    
+       Absolute.Renly.Rate.Range.Undigable
     总结：返回真实度：性：绝对结束
-    
+
     涵数：绝对还原
-        瑞蕾：Renly rely 
-        
+        瑞蕾：Renly rely
+
         瑞尔还原：地球 外星
         1）古代事件转制：刑 （小人相互反射) 被刑
         2）现代事件问题：疼合适类似于洛克梅根都经历老掌沟奇遇记
 
         最终声明形式
-        metric            
+        metric
     总结
   总结.结果
 ShamedArentFramedAlfed：10-20 -- 10-21
@@ -110,16 +108,16 @@ ShamedArentFramedAlfed：10-20 -- 10-21
             Ride dick cku L sz
             底格里斯河
         磁欧石
-        水晶球type colorfirst 
+        水晶球type colorfirst
         Flag：阴毒过期 -<>- 一浪~~
-            Metriwhich 
+            Metriwhich
             Color
 FrameSleep：10-22 -- 10-24
-    
+
     TortureRant:
         Portal.Left
         Portal.Get
-        Portal.FormerLife.Tinywhat      
+        Portal.FormerLife.Tinywhat
         Portal.FormerLife.SufferKint
         Portal.FormerLife
     SelfTorture
@@ -148,48 +146,48 @@ FrameSleep7：10-26
         waste
         kansai notice first kause of  absolute pure environment plant
         metricthinking：krystal diamond
-    skin hand touch overall 
+    skin hand touch overall
         under maybe first
         hand little finger to thumb
         keyboard unknown finger
-    Cos    
-        light and beam reflect 
+    Cos
+        light and beam reflect
     Crystal
         study communication
     Enzo Crytech
         leon in
-        Rational 
+        Rational
             Rose
             Ross
             Rosetta
-    
+
     Wireless 0 1 trangle is removed and the bottom line
 4plus endo 20-27
     Muscle:
         belly dick turtle
 Jesus Krystal + 7 PM 12:11 10/28
-    主牢烙 方格蚩 ruler 
+    主牢烙 方格蚩 ruler
     牙色忘
 国际招魂 10-29
     evocation
     zither
-    维今变形记 日寸 
-        self-abortion         
+    维今变形记 日寸
+        self-abortion
         dice after bornt
 10-31 will be
     Solid.Metricwitch.Language.Effect
 10-3~2 日 困
     上古正神
 Geother-Post-longfumarket：11-7
-    突厥语 
+    突厥语
         键盘：重复连贯 链方向： 上 下 做 油
         2,3个字母组成一个
 11-9 to  11
-    平衡字 
+    平衡字
     朝右边的
-    撮苤紫South Pole 
-    整个字 偏旁部首 索来德 方向 
-    左右 上下 对子 后司 
+    撮苤紫South Pole
+    整个字 偏旁部首 索来德 方向
+    左右 上下 对子 后司
         下
         上
     中英文字母偏旁部首 Geographic Metric-Thinking
@@ -199,26 +197,26 @@ Geother-Post-longfumarket：11-7
              上 我
     语系：欧罗 突厥 汉藏
 
-        华阴音 俄语  bi b 
+        华阴音 俄语  bi b
         上下赌点,分离,连贯,IsDirect,IsDashed,IsArc
-        Persvision Position-Towards 大|小|写  
+        Persvision Position-Towards 大|小|写
         Dd Ll
         反写都还顺手的
-        qp 
+        qp
         手写 打字
 
-    定于沌湟 Testiclefish 雪性毬  妓鼠Lz小天鹅
+    定于沌湟 Testiclefish 雪性毬 &'+ wLz小天沿
 
     Framily:Extreme | Absolute
         Virginal as physcial criteria
-        Isdirty?contest:absolute           
+        Isdirty?contest:absolute
         bar-level-boundary
         abuse  swear
-            滥用; 妄用; 虐待; 辱骂; 恶语;
+            滥用; 妄用; 虐待; 恶语;
         Sexcast as PBC Creal
 
     战争折
-        Kause 古罗马 彼得大帝 二战
+        Kause 古l*^^ 彼得大帝 二战
     Anal holes tissues color flip times
     世纪元 地理 书写 叫法
     metric vesaid solid odin timesnical 区域叫法：省 郡 州
@@ -238,3 +236,5 @@ Geother-Post-longfumarket：11-7
     元素 属性 文字 偏旁部首
 11-13
     电机厂 气铅石 R苯奙jap an 泥子
+
+=cQ R~+^ 
